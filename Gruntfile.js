a2340c8dfc806a53f364502db9cdebd699c9c02c
GavMan1995 <gavyncaldwell@gmail.com>
2017-03-14 20:35:46 -0600

    new scss defaults

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,47 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
 			dist: {
-				files: {
-					'application.css' : 'styles.scss'
-				}
+        options: {
+          noCache: true,
+          style: 'compressed',
+          sourcemap: 'none'
+        },
+
+				files: {'app/application.css' : 'styles.scss'}
 			}
 		},
+
+		connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: '*',
+          base: 'app/'
+        }
+      }
+    },
+
 		watch: {
 			css: {
 				files: '**/*.scss',
 				tasks: ['sass']
 			}
-		}
+		},
+
+		surge: {
+			'Your Project Name': {
+				options: {
+					project: 'app/',
+					domain: 'navfit-styles.surge.sh'
+				}
+			}
+		},
+		
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.loadNpmTasks('grunt-surge');
+	grunt.loadNpmTasks('grunt-contrib-connect'); 
+	grunt.registerTask('default', ['connect', 'watch']);
+	grunt.registerTask('deploy', ['sass', 'surge']);
 }
